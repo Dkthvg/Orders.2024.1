e4aa8d8539bb46b62b78930a8716d9f1287ad4a7
DIANA KATHERINE VELASQUEZ GUTIERREZ <diana5847@hotmail.com>
2024-05-02 21:00:12 -0500

    Creation of reading countries from the frontEnd

--- Orders/Orders.Frontend/Repositories/Repository.cs
+++ Orders/Orders.Frontend/Repositories/Repository.cs
@@ -1,27 +1,60 @@
 ﻿
+using System.Text;
+using System.Text.Json;
+
 namespace Orders.Frontend.Repositories
 {
     public class Repository : IRepository
     {
        private readonly HttpClient _httpClient;
+        private JsonSerializerOptions _jsonDefaultOptions => new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
         
         public Repository(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
-        public Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
+        //Get para listar paises
+        public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
         {
-            throw new NotImplementedException();
+            var responseHttp = await _httpClient.GetAsync(url);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswer<T>(responseHttp);
+                return new HttpResponseWrapper<T>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<T>(default, true, responseHttp);
         }
 
-        public Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
+
+        //Post para listar paises
+        public async Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
         {
-            throw new NotImplementedException();
+            var messageJson = JsonSerializer.Serialize(model);//Deserializar el modelo que se recibe
+            var messageContent = new StringContent(messageJson, Encoding.UTF8, "aplication/json");//Codificación del modelo
+            var responseHttp = await _httpClient.PostAsync(url, messageContent);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
         }
 
-        public Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model)
+        public async Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model)
         {
-            throw new NotImplementedException();
+            var messageJson = JsonSerializer.Serialize(model);//Deserializar el modelo que se recibe
+            var messageContent = new StringContent(messageJson, Encoding.UTF8, "aplication/json");//Codificación del modelo
+            var responseHttp = await _httpClient.PostAsync(url, messageContent);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswer<TActionResponse>(responseHttp);
+                return new HttpResponseWrapper<TActionResponse>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<TActionResponse>(default, true, responseHttp);
+        }
+
+        private async Task<T> UnserializeAnswer<T>(HttpResponseMessage responseHttp)
+        {
+            var response = await responseHttp.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(response, _jsonDefaultOptions)!;
         }
     }
 }
